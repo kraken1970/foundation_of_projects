feb50d86a7fe63b38a7adaaa5380e38777d57700
kraken1970 <kraken@hi.ru>
2018-04-16 14:08:35 +0300

    for start all project

---
+++ src/assets/JS/app.js
@@ -0,0 +1,14 @@
+console.log('HI!!!!!!!!!!');
+
+// ( ()=> {
+//   const activeTopMenu = document.querySelector('#menu');
+//   const topMenu = document.querySelector('.top_nav--menu');
+//   const mainTitle = document.querySelector('.title-subtitle');
+
+//   activeTopMenu.onclick  = (event) =>  {
+//     event.preventDefault();
+//     topMenu.classList.toggle('responsive');
+//     mainTitle.classList.toggle('responsive-title');
+//   }
+// })(); 
+// из DER_company
